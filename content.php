bd9ff461c6c58403ab9bcc80f9bbaa3784f540bf
Jacob Pariseau <jacob@jacobpariseau.com>
2016-10-11 20:51:06 -0600

    Fix permalinks

--- content.php
+++ content.php
@@ -1,15 +1,22 @@
 <div class="blog-post">
-<h2 class="blog-post-title"><a class="h2" href=
-"%3C?php%20the_permalink();%20?%3E"><?php the_title(); ?></a></h2>
-<p class="blog-post-meta"><?php the_date(); ?> by <a href=
-"#"><?php the_author(); ?></a> • <a href=
-"%3C?php%20comments_link();%20?%3E"><?php
-                printf( _nx( 'One Comment', '%1$s Comments', get_comments_number(), 'comments title', 'textdomain' ), number_format_i18n(                                               get_comments_number() ) ); ?></a></p>
-<?php if ( has_post_thumbnail() ) {?>
-<div class="row">
-<div class="col-md-4">
-<?php   the_post_thumbnail('thumbnail'); ?></div>
-<div class="col-md-6"><?php the_excerpt(); ?></div>
+  <h2 class="blog-post-title">
+    <a class="h2" href="<?php the_permalink(); ?>">
+      <?php the_title(); ?>
+    </a>
+  </h2>
+  <p class="blog-post-meta">
+    <?php the_date(); ?> by
+    <a href="#"><?php the_author(); ?></a> •
+    <a href="<?php comments_link(); ?>">
+      <?php
+        printf( _nx( 'One Comment', '%1$s Comments', get_comments_number(), 'comments title', 'textdomain' ), number_format_i18n(get_comments_number())); ?>
+      </a>
+  </p>
+  <?php if ( has_post_thumbnail() ) {?>
+  <div class="row">
+    <div class="col-md-4"><?php   the_post_thumbnail('thumbnail'); ?></div>
+    <div class="col-md-6"><?php the_excerpt(); ?></div>
+  </div>
+  <?php } else { ?><?php the_excerpt(); ?><?php } ?>
 </div>
-<?php } else { ?><?php the_excerpt(); ?><?php } ?></div>
 <!-- /.blog-post -->
