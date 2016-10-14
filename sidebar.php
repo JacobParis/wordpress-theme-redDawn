932326b64176530f69935fbd36f90f50be73b76b
Jacob Pariseau <jacob@jacobpariseau.com>
2016-10-13 22:35:21 -0600

    Change Colours and remove descriptions

--- sidebar.php
+++ sidebar.php
@@ -1,9 +1,5 @@
 <?php if ( is_active_sidebar( 'home_1' ) ) : ?>
 
-<div class="blog-sidebar-overlay hidden-xs">
-</div>
-<div class="blog-sidebar-overlay-xs visible-xs">
-</div>
 <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
 	<div class="row">
 			<?php dynamic_sidebar( 'home_1' ); ?>
