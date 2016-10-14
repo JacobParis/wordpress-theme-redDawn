223153f5140cf138dd42c16152a07d0a0f192704
Jacob Pariseau <jacob@jacobpariseau.com>
2016-10-13 20:05:22 -0600

    Shaded footer

--- sidebar.php
+++ sidebar.php
@@ -2,6 +2,8 @@
 
 <div class="blog-sidebar-overlay hidden-xs">
 </div>
+<div class="blog-sidebar-overlay-xs visible-xs">
+</div>
 <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
 	<div class="row">
 			<?php dynamic_sidebar( 'home_1' ); ?>
