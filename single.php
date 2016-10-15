bced1be897ad5e4729637432f869ed1657e32429
Jacob Pariseau <jacob@jacobpariseau.com>
2016-10-14 22:23:38 -0600

    Removed comment support

--- single.php
+++ single.php
@@ -2,13 +2,10 @@
 	<div class="row">
 		<div class="col-sm-12">
 			<?php
-				if ( have_posts() ) : while ( have_posts() ) : the_post();
-					get_template_part( 'content-single', get_post_format() );
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				endwhile; endif;
-			?>
+                if ( have_posts() ) : while ( have_posts() ) : the_post();
+                    get_template_part( 'content-single', get_post_format() );
+                endwhile; endif;
+            ?>
 		</div> <!-- /.col -->
 	</div> <!-- /.row -->
 <?php get_footer(); ?>
