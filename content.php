1a4ba59a330ef8602657f9c17a10894b62cf2e82
Jacob Pariseau <jacob@jacobpariseau.com>
2016-11-12 20:06:06 -0700

    Fix page view

--- content.php
+++ content.php
@@ -11,8 +11,8 @@
   <?php if ( has_post_thumbnail() ) {?>
   <div class="row">
     <div class="col-md-4"><?php   the_post_thumbnail('thumbnail'); ?></div>
-    <div class="col-md-6"><?php the_excerpt(); ?></div>
+    <div class="col-md-6"><?php the_content(); ?></div>
   </div>
-  <?php } else { ?><?php the_excerpt(); ?><?php } ?>
+  <?php } else { ?><?php the_content(); ?><?php } ?>
 </div>
 <!-- /.blog-post -->
